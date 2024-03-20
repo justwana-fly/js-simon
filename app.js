0eb9c18bb19098e3136d3f9a93e14750c9ebd58c
justwana-fly <nicolaibarbarasa@gmail.com>
2024-03-20 15:45:23 +0100

    i numeri scompaiono dopo 5 sec

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 // Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
-
-// Da lì parte un timer di 30 secondi.
-
-// Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
-
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
-
 function generaNumeriCasuali() {
     const randomsNum = [];
     while (randomsNum.length < 5) {
@@ -19,4 +11,21 @@ function generaNumeriCasuali() {
 }
 
 const numeriCasuali = generaNumeriCasuali();
-console.log("Numeri casuali generati:", numeriCasuali);
+
+const mioId = document.getElementById("output")
+mioId.innerHTML = numeriCasuali
+
+// Da lì parte un timer di 5 secondi.
+setTimeout(function() {
+    // Rimuovi i numeri generati
+    numeriCasuali.length = 0;
+    mioId.innerHTML = ""; // Rimuovi il contenuto HTML
+}, 5000); // timer 5 sec
+
+
+// Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
